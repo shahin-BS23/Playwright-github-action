0b7b18e9bf18f6a59586448dc268db6c1b04b937
shahin-BS23 <Shahin.Shanaous@theiia.org>
2023-10-14 15:22:17 +0600

    Github action

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,16 +4,20 @@ test.describe('Test for github action',async () => {
   
   test('Navigate to the login page',async ({page}) => {
     await page.goto('https://www.saucedemo.com/');
-    expect(await page.url()).toEqual('https://www.saucedemo.com/');
+    expect(page.url()).toEqual('https://www.saucedemo.com/');
   });
 
   test('Do login',async({page})=>{
     await page.goto('https://www.saucedemo.com/');
     await page.locator('#user-name').fill('standard_user');
     await page.locator('#password').fill('secret_sauce');
-    await page.locator('#login-button').click;
+    await page.locator('#login-button').click();
 
-    expect(await page.locator(',shopping_cart_link').isVisible);
+    await page.waitForLoadState('networkidle');
+
+    console.log(page.locator('#shopping_cart_container'));
+
+    expect(page.locator('.shopping_cart_link')).toBeVisible();
   })
 
 })
