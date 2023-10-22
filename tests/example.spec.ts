37aab3e51ab3507ab3026cf57355689916790de3
shahin-BS23 <shahin.brainstation-23@gmail.com>
2023-10-22 20:52:39 +0600

    Update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -18,13 +18,5 @@ test.describe('Test for github action',async () => {
     console.log(page.locator('#shopping_cart_container'));
 
     expect(page.locator('.shopping_cart_link')).toBeVisible();
-  })
-
-  test.skip('Skip the test',async () => {
-    
-  })
-
-  test('I am going to fail',async () => {
-    test.fail();
-  })
+  });
 })
